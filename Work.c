c6299fc85a55fbfe1c85f28d4348bd4d051c9e79
Manish Panwar <panwarmanish2710@gmail.com>
2024-06-21 12:19:10 +0530

    Add files via upload

---
+++ Work.c
@@ -0,0 +1,357 @@
+#include <stdio.h>
+int fare_cal_1_2()
+{
+    int fare = 3, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_1_3()
+{
+    int fare = 5, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_1_4()
+{
+    int fare = 8, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_1_5()
+{
+    int fare = 10, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_1_6()
+{
+    int fare = 13, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_2_3()
+{
+    int fare = 3, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_2_4()
+{
+    int fare = 5, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_2_5()
+{
+    int fare = 8, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_2_6()
+{
+    int fare = 10, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_3_4()
+{
+    int fare = 5, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_3_5()
+{
+    int fare = 5, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_3_6()
+{
+    int fare = 8, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_4_5()
+{
+    int fare = 5, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_4_6()
+{
+    int fare = 8, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int fare_cal_5_6()
+{
+    int fare = 5, person;
+    printf("                   Number of Person :- ");
+    scanf("%d", &person);
+    int new_fare;
+    new_fare = person * fare;
+    printf("        <------------------------------------------------------>\n");
+    printf("                              Welcome\n");
+    printf("                                 To\n");
+    printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+    printf("                      Number of Persons :- %d\n", person);
+    printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+    printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+    printf("        <------------------------------------------------------>\n");
+}
+int main()
+{
+    int press = 1, i = 0;
+    do
+    {   
+        printf("                       Press 1 To Continue.");
+        scanf("%d",&press);
+        if (press == 1)
+        {
+            printf("<--------------- Welcome To the Service --------------->\n");
+
+            printf("                   1. Summer Hill\n");
+            printf("                   2. Balugunj\n");
+            printf("                   3. Crossing\n");
+            printf("                   4. Hotel 103\n");
+            printf("                   5. Vidhansbha\n");
+            printf("                   6. Old Bus Stand\n\n");
+
+            printf("<--------------- Your Destination --------------->\n\n");
+            int ask1, ask2;
+            printf("                    From :- ");
+            scanf("%d", &ask1);
+            printf("                    To :- ");
+            scanf("%d", &ask2);
+
+            // <------------------ 1 to 6 && 6 to 1 ------------------>
+            if (ask1 == 1 && ask2 == 2 || ask1 == 2 && ask2 == 1)
+            {
+                fare_cal_1_2();
+            }
+            else if (ask1 == 1 && ask2 == 3 || ask1 == 3 && ask2 == 1)
+            {
+                fare_cal_1_3();
+            }
+            else if (ask1 == 1 && ask2 == 4 || ask1 == 4 && ask2 == 1)
+            {
+                fare_cal_1_4();
+            }
+            else if (ask1 == 1 && ask2 == 5 || ask1 == 5 && ask2 == 1)
+            {
+                fare_cal_1_5();
+            }
+            else if (ask1 == 1 && ask2 == 6 || ask1 == 6 && ask2 == 1)
+            {
+                fare_cal_1_6();
+            }
+
+            // <------------------ 2 to 6 && 6 to 2 ------------------>
+            else if (ask1 == 2 && ask2 == 3 || ask1 == 3 && ask2 == 2)
+            {
+                fare_cal_2_3();
+            }
+            else if (ask1 == 2 && ask2 == 4 || ask1 == 4 && ask2 == 2)
+            {
+                fare_cal_2_4();
+            }
+            else if (ask1 == 2 && ask2 == 5 || ask1 == 5 && ask2 == 2)
+            {
+                fare_cal_2_5();
+            }
+            else if (ask1 == 2 && ask2 == 6 || ask1 == 6 && ask2 == 2)
+            {
+                fare_cal_2_6();
+            }
+
+            // <------------------ 3 to 6 && 6 to 3 ------------------>
+            else if (ask1 == 3 && ask2 == 4 || ask1 == 4 && ask2 == 3)
+            {
+                fare_cal_3_4();
+            }
+            else if (ask1 == 3 && ask2 == 5 || ask1 == 5 && ask2 == 3)
+            {
+                fare_cal_3_5();
+            }
+            else if (ask1 == 3 && ask2 == 6 || ask1 == 6 && ask2 == 3)
+            {
+                fare_cal_3_6();
+            }
+
+            // <------------------ 4 to 6 && 6 to 4 ------------------>
+            else if (ask1 == 4 && ask2 == 5 || ask1 == 5 && ask2 == 4)
+            {
+                fare_cal_4_5();
+            }
+            else if (ask1 == 4 && ask2 == 6 || ask1 == 6 && ask2 == 4)
+            {
+                fare_cal_4_6();
+            }
+
+            // <------------------ 5 to 6 && 6 to 5 ------------------>
+            else if (ask1 == 5 && ask2 == 6 || ask1 == 6 && ask2 == 5)
+            {
+                fare_cal_5_6();
+            }
+            else
+            {
+                int fare = 5, person;
+                printf("                   Number of Person :- ");
+                scanf("%d", &person);
+                int new_fare;
+                new_fare = person * fare;
+                printf("        <------------------------------------------------------>\n");
+                printf("                              Welcome\n");
+                printf("                                 To\n");
+                printf("                   **** SHIMLA BUS SERVICE ****\n\n");
+                printf("                      Number of Persons :- %d\n", person);
+                printf("                      Your fare is %d * %d :- %d.\n", person, fare, new_fare);
+                printf("                   ~~~~ Have a Safe Journey ~~~~\n");
+                printf("        <------------------------------------------------------>\n");
+            }
+        }
+
+        i++;
+    } while (i < 1000);
+    return 0;
+}
